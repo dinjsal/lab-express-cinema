0be6233d98907202e7694e1069da8cefaf7f8e93
Dinah Salazar <dinahsalazar@gmail.com>
2023-10-21 21:25:59 +0900

    Test again

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const MONGODB_URI = PROCESS.ENV.MONGODB_URI;
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
 
 module.exports = router;
