c751a5c5d44caf41fd36658b6dce15bcfd64d78f
Dinah Salazar <dinahsalazar@gmail.com>
2023-10-22 08:50:06 +0900

    Complete iteration 4, with deleted mongodb secret key

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const MONGODB_URI = PROCESS.ENV.MONGODB_URI;
+const MONGO_URI =
+  
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
 router.get("/", (req, res, next) => res.render("index"));
 
+router.get("/movies", (req, res) => {
+  Movie.find()
+    .then((response) => {
+      res.render("movies", { movies: response });
+    })
+    .catch((err) => console.log(err));
+});
+
+// Single movie GET with back to homepage link
+
+router.get("/movie/:id", (req, res) => {
+  const bookId = req.params.id;
+  Movie.findById(bookId)
+    .then((response) => {
+      res.render("more", { movies: response });
+    })
+    .catch((err) => console.log(err));
+});
+
+//
 module.exports = router;
