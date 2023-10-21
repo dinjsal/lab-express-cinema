0e55d48f3a46890e9df643a6b076685bd25fdde9
Dinah Salazar <dinahsalazar@gmail.com>
2023-10-22 08:51:25 +0900

    Complete iteration 4, without mongodb secret key. Redo lab

---
+++ models/Movie.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema(
+  {
+    title: { type: String },
+    director: { type: String },
+    stars: [String],
+    image: { String },
+    description: { type: String },
+    showtimes: [String],
+  },
+  { timestamps: true }
+);
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
